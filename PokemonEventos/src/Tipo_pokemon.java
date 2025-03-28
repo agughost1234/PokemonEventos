749a95fe8e2197cbe6ca3131fd8cf7c4f3c2e05c
JMCRmeteorito <jose.castano.rojas@correounivalle.edu.co>
2025-03-27 21:23:53 -0500

    Removal of enums
    Eradication of enums type attack and pokémon

--- PokemonEventos/src/Tipo_pokemon.java
+++
@@ -1,5 +0,0 @@
-public enum Tipo_pokemon {
-    //Example of types
-    Normal, Fire, Water, Electric, Grass, Ice, Fighting, Poison, 
-    Ground, Flying, Psychic, Bug, Rock, Ghost, Dragon, Dark, Steel, Fairy;
-}
