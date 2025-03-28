ab96b99983ca96f4d321dd593ca9f10d364128f3
JMCRmeteorito <jose.castano.rojas@correounivalle.edu.co>
2025-03-27 20:41:17 -0500

    Enum create file
    This is the enum for attack of the pokémon

---
+++ PokemonEventos/src/Ataques_Word.java
@@ -0,0 +1,13 @@
+public enum Ataques_Word {
+    //Example of attacks
+    Fireball, Thunderbolt, Icebeam, Earthquake, Tsunami, 
+    Tornado, Hurricane, Volcano, Meteor, Blackhole, 
+    Supernova, Bigbang, Bigcrunch, Bigrip, Bigfreeze, 
+    Bigburn, Bigshock, Bigfrost, Bigquake, Bigwave, 
+    Bigstorm, Bigeruption, Bigmeteor, Bigblackhole, 
+    Bigsupernova, Bigbangbang, Bigcrunchcrunch, Bigriprip, 
+    Bigfreezefreeze, Bigburnburn, Bigshockshock, Bigfrostfrost,
+    Bigquakequake, Bigwavewave, Bigstormstorm, 
+    Bigeruptioneruption, Bigmeteormeteor, Bigblackholeblackhole, 
+    Bigsupernovasupernova;
+}
