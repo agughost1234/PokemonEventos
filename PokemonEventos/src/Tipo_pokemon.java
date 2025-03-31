188df72fd8f35b7e7e361d09cdcae5ea89d9004d
JMCRmeteorito <jose.castano.rojas@correounivalle.edu.co>
2025-03-31 15:05:36 -0500

    Cambios a un solo enum
    Enum type attack and pokémon are in one enum

--- PokemonEventos/src/Tipo_pokemon.java
+++
@@ -1,7 +0,0 @@
-public enum Tipo_pokemon {
-    // example pokemons type 
-    NORMAL, FIRE, WATER, ELECTRIC, GRASS, ICE, FIGHTING, POISON, GROUND, FLYING,
-    PSYCHIC, BUG, ROCK, GHOST, DRAGON, DARK, STEEL, FAIRY
-    
-    
-}
