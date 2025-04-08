4ea73e9bee652fb710be3add6dae210e72cbf716
agughost1234 <agudelo.samuel@correounivalle.edu.co>
2025-04-07 22:11:49 -0500

    entrenador cambios de inicializar pokemones

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -5,6 +5,7 @@ public class Entrenador {
     
     private final String nombre;
     private final ArrayList<Pokemon> equipo = new ArrayList<>();
+    
 
     public Entrenador(String nombre) {
         this.nombre = nombre;
@@ -32,9 +33,7 @@ public class Entrenador {
        
     }
 
-    //flujo recomendado primer paso crear entrenador con la clase Entrenador y luego capturar pokemones
-    //capturarPokemon() será un método de entrenador
-    //capturarPokemon() se encargará de capturar pokemones y agregarlos al equipo
+
     
 //Factory method para capturar un entrenador por consola sin necesidad de instanciar un objeto con new
     public static Entrenador capturarEntrenador() {
@@ -48,122 +47,34 @@ public class Entrenador {
         System.out.println("Bienvenido " + nombre + "!");
         System.out.println("¡Prepárate para capturar Pokémons y convertirte en un maestro Pokémon!");
         System.out.println("************************************");
-        System.out.println("Recuerda que solo puedes capturar 3 pokemones por equipo.");
-        System.out.println("************************************");
         System.out.println("¡Comencemos!");
         System.out.println("************************************");
         return new Entrenador(nombre);
     }
 
     public void capturarPokemon() {
-        
-        Scanner scanner = new Scanner(System.in);
-    
-        while (true) {
-            if (equipo.size() == 3) {
-                System.out.println("No puedes capturar más pokemones, tu equipo está completo.");
-                System.out.println("************************************");
-                break;
-            }
-            else {
-                System.out.println("Deseas capturar un Pokémon? (si/no), si no entonces se formará el equipo aleatoriamente");
-                String respuesta = scanner.nextLine().toLowerCase();
-                System.out.println("************************************");
-                if (respuesta.equals("si") ) {
-                    int cantidadPokemones = 4;
-                    while (cantidadPokemones > 3 || cantidadPokemones < 1) {
-                        System.out.println("¿Cuántos pokemones quieres capturar? (entre 1 y 3): ");
-                        cantidadPokemones = scanner.nextInt();
-                        scanner.nextLine(); // Limpiar el buffer del scanner
-                        if (cantidadPokemones > 3 || cantidadPokemones < 1) {
-                            System.out.println("El número de pokemones debe estar entre 1 y 3.");
-                            
-                        }
-                        
-                        System.out.println("************************************");
+    // preguntar si quiere crear equipo manualmente o aleatoriamente
+    boolean equipoAleatorio = false;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("¿Quieres capturar un pokemon? (s/n)");
+    String respuesta = scanner.nextLine().trim().toLowerCase();
+    while (true) {
+        if (respuesta.equals("s")) {
+            break;
+        } else if (respuesta.equals("n")) {
+            equipoAleatorio = true;
+            break;
+        } else {
+            System.out.println("Respuesta no válida. Por favor, ingresa 's' o 'n'.");
+            respuesta = scanner.nextLine().trim().toLowerCase();
+        }
+    }
+  for (int i = 0; i < 3; i++) {
+      //  equipo.add(Pokemon.instanciarPokemon(equipoAleatorio));
+      
+  }
 
-                    }
-                    for (int i = 0; i < cantidadPokemones; i++) {
-                        System.out.println("Ingrese el nombre del pokemon: ");
-                        String nombrePokemon = scanner.nextLine().trim(); // trim() para eliminar espacios en blanco
-                        System.out.println("************************************");
-                        System.out.println("Tipos de pokemon disponibles: ");
-                        int contador = 1;
-                        for (Tipo_ataque_pokemon tipo : Tipo_ataque_pokemon.values()) {
-                            System.out.println(contador + ". " + tipo);
-                            contador++;
-                            
-                        }
-                        System.out.println("************************************");
-                        int tipoPokemon = 0;
-                        while (tipoPokemon < 1 || tipoPokemon > Tipo_ataque_pokemon.values().length) {
-                            System.out.println("Ingrese el tipo de pokemon (1-" + Tipo_ataque_pokemon.values().length + "): ");
-                            tipoPokemon = scanner.nextInt(); // tipoPokemon es un int
-                            scanner.nextLine(); // Limpiar el buffer del scanner
-                            if ( tipoPokemon < 1 || tipoPokemon > Tipo_ataque_pokemon.values().length) {
-                                System.out.println("El tipo de pokemon debe estar entre 1 y " + Tipo_ataque_pokemon.values().length + ".");
-                                
-                            }
-                            System.out.println("************************************");
-                            
-                        }
-                        Tipo_ataque_pokemon tipoPokemon_ = Tipo_ataque_pokemon.values()[tipoPokemon - 1]; // convertir a enum
-                        int hpPokemon = 0;
-                        while (hpPokemon < 100 || hpPokemon > 200) {
-                            System.out.println("Ingrese el hp del pokemon (entre 100 y 200): ");
-                            hpPokemon = scanner.nextInt();
-                            scanner.nextLine(); // Limpiar el buffer del scanner
-                            if (hpPokemon < 100 || hpPokemon > 200) {
-                                System.out.println("El hp debe estar entre 100 y 200.");
-                            
-                            }
-                            System.out.println("************************************");
-
-                        }
-                        Pokemon pokemon = new Pokemon(nombrePokemon, tipoPokemon_, null, hpPokemon);
-                        agregarPokemonEquipo(pokemon); 
-                    }
-                    nombrarEquipo();
-                  
-                    break;
-                   
-                }
-                else if (respuesta.equals("no")) {
-                    System.out.println("Se formará el equipo aleatoriamente.");
-                    System.out.println("************************************");
-                    Random random = new Random();
-                    int cantidadPokemon = random.nextInt(3) + 1; // Genera un número aleatorio entre 1 y 3
-                    for (int i = 0; i < cantidadPokemon; i++) {
-                        System.out.println("Ingrese el nombre del pokemon: ");
-                        String nombrePokemon = scanner.nextLine().trim(); // trim() para eliminar espacios en blanco
-                        System.out.println("************************************");
-                        int tipoPokemon = random.nextInt(Tipo_ataque_pokemon.values().length);   
-                        Tipo_ataque_pokemon tipoPokemon_ = Tipo_ataque_pokemon.values()[tipoPokemon]; // convertir a enum
-                        int hpPokemon = random.nextInt(200-100+1) + 100; // Genera un número aleatorio entre 1 y 100
-                        Pokemon pokemon = new Pokemon(nombrePokemon, tipoPokemon_, null, hpPokemon);
-                        agregarPokemonEquipo(pokemon);
-                    }
-                    nombrarEquipo();
-                    break;
-                } else {
-                    System.out.println("Respuesta no válida. Por favor, ingresa 'si' o 'no'.");
-                    
-            }
-        
-            }
-           
-    }
-       
-        
-       
-    }
-    //agregar un pokemon al equipo
-    public void agregarPokemonEquipo(Pokemon pokemon) {
-        // cumplir el tamaño de la lista pokemon
-        if (equipo.size() <= 3) {
-            equipo.add(pokemon);
-        } 
-    }
+}
 
     
 }
