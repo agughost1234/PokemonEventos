77dece3e1b85ff67b91007a8e3f8ce9535ec4e60
agughost1234 <agudelo.samuel@correounivalle.edu.co>
2025-04-09 18:24:56 -0500

    clase entrenador esperemos terminada

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,69 +1,17 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-
+import java.util.Random;
 public class Entrenador extends SerVivo{
     private final ArrayList<Pokemon> equipo = new ArrayList<>();
-    private static final ArrayList<Entrenador> entrenadores = new ArrayList<>();
-    private static final Scanner scanner = new Scanner(System.in);
 
     public Entrenador(String nombre) {
         super(nombre);
-        entrenadores.add(this);
-
-      
     }
 
-    static {
-
-    // // Entrenador 1: Ash
-    // Entrenador ash = new Entrenador("Ash Ketchum");
-    // ash.getEquipo().add(new Pokemon("Charizard", Tipo_ataque_pokemon.FUEGO, Ataque.obtenerLista(Tipo_ataque_pokemon.FUEGO), 120f));
-    // ash.getEquipo().add(new Pokemon("Pikachu", Tipo_ataque_pokemon.ELECTRICO, Ataque.obtenerLista(Tipo_ataque_pokemon.ELECTRICO), 100f));
-    // ash.getEquipo().add(new Pokemon("Squirtle", Tipo_ataque_pokemon.AGUA, Ataque.obtenerLista(Tipo_ataque_pokemon.AGUA), 95f));
-    // entrenadores.add(ash);
-
-    // // Entrenador 2: Misty
-    // Entrenador misty = new Entrenador("Misty");
-    // misty.getEquipo().add(new Pokemon("Staryu", Tipo_ataque_pokemon.AGUA, Ataque.obtenerLista(Tipo_ataque_pokemon.AGUA), 90f));
-    // misty.getEquipo().add(new Pokemon("Vaporeon", Tipo_ataque_pokemon.AGUA, Ataque.obtenerLista(Tipo_ataque_pokemon.AGUA), 110f));
-    // misty.getEquipo().add(new Pokemon("Bellossom", Tipo_ataque_pokemon.PLANTA, Ataque.obtenerLista(Tipo_ataque_pokemon.PLANTA), 100f));
-    // entrenadores.add(misty);
-
-    // // Entrenador 3: Brock
-    // Entrenador brock = new Entrenador("Brock");
-    // brock.getEquipo().add(new Pokemon("Onix", Tipo_ataque_pokemon.TIERRA, Ataque.obtenerLista(Tipo_ataque_pokemon.TIERRA), 130f));
-    // brock.getEquipo().add(new Pokemon("Graveler", Tipo_ataque_pokemon.TIERRA, Ataque.obtenerLista(Tipo_ataque_pokemon.TIERRA), 105f));
-    // brock.getEquipo().add(new Pokemon("Magcargo", Tipo_ataque_pokemon.FUEGO, Ataque.obtenerLista(Tipo_ataque_pokemon.FUEGO), 95f));
-    // entrenadores.add(brock);
-
-    // // Entrenador 4: Gary
-    // Entrenador gary = new Entrenador("Gary");
-    // gary.getEquipo().add(new Pokemon("Arcanine", Tipo_ataque_pokemon.FUEGO, Ataque.obtenerLista(Tipo_ataque_pokemon.FUEGO), 125f));
-    // gary.getEquipo().add(new Pokemon("Blastoise", Tipo_ataque_pokemon.AGUA, Ataque.obtenerLista(Tipo_ataque_pokemon.AGUA), 120f));
-    // gary.getEquipo().add(new Pokemon("Electivire", Tipo_ataque_pokemon.ELECTRICO, Ataque.obtenerLista(Tipo_ataque_pokemon.ELECTRICO), 110f));
-    // entrenadores.add(gary);
-
-    // // Entrenador 5: Jessie (Equipo Rocket)
-    // Entrenador jessie = new Entrenador("Jessie");
-    // jessie.getEquipo().add(new Pokemon("Wobbuffet", Tipo_ataque_pokemon.TIERRA, Ataque.obtenerLista(Tipo_ataque_pokemon.TIERRA), 100f));
-    // jessie.getEquipo().add(new Pokemon("Dustox", Tipo_ataque_pokemon.PLANTA, Ataque.obtenerLista(Tipo_ataque_pokemon.PLANTA), 95f));
-    // jessie.getEquipo().add(new Pokemon("Seviper", Tipo_ataque_pokemon.TIERRA, Ataque.obtenerLista(Tipo_ataque_pokemon.TIERRA), 105f));
-    // entrenadores.add(jessie);
-
-    // // Entrenador 6: James (Equipo Rocket)
-    // Entrenador james = new Entrenador("James");
-    // james.getEquipo().add(new Pokemon("Weezing", Tipo_ataque_pokemon.TIERRA, Ataque.obtenerLista(Tipo_ataque_pokemon.TIERRA), 105f));
-    // james.getEquipo().add(new Pokemon("Cacnea", Tipo_ataque_pokemon.PLANTA, Ataque.obtenerLista(Tipo_ataque_pokemon.PLANTA), 100f));
-    // james.getEquipo().add(new Pokemon("Mime Jr.", Tipo_ataque_pokemon.ELECTRICO, Ataque.obtenerLista(Tipo_ataque_pokemon.ELECTRICO), 90f));
-    // entrenadores.add(james);
-
-
-    }
 
 
     public ArrayList<Pokemon> getEquipo() {
         return equipo;
-
     }
 
     public void nombrarEquipo() {
@@ -76,16 +24,11 @@ public class Entrenador extends SerVivo{
                 System.out.println("Tipo del pokemon: " + pokemon.getTipo());
                 System.out.println("Hp del pokemon: " + pokemon.getHp());
                 System.out.println("************");
+                
         }
        
     }
 
-    public void mostrarEntrenadores() {
-        System.out.println("Entrenadores disponibles:");
-        for (Entrenador entrenador : entrenadores) {
-            entrenador.nombrarEquipo();
-        }
-    }
 
     
 //Factory method para capturar un entrenador por consola sin necesidad de instanciar un objeto con new
@@ -102,28 +45,20 @@ public class Entrenador extends SerVivo{
         System.out.println("************");
         System.out.println("¡Comencemos!");
         System.out.println("************");
-        
-        Entrenador entrenador = new Entrenador(nombre);
-        // Llamar al método para capturar Pokémon
-        entrenador.capturarPokemon();
-        // Llamar al método para nombrar el equipo
-        entrenador.nombrarEquipo();
-        return entrenador;
+        return new Entrenador(nombre);
     }
-
-
 
     public void capturarPokemon() {
     // preguntar si quiere crear equipo manualmente o aleatoriamente
-    boolean equipoAleatorio = false;
+    boolean equipoManual = true;
     Scanner scanner = new Scanner(System.in);
-    System.out.println("¿Quieres capturar un pokemon? (s/n)");
+    System.out.println("¿Quieres capturar un pokemon de forma manual? (s/n)");
     String respuesta = scanner.nextLine().trim().toLowerCase();
     while (true) {
         if (respuesta.equals("s")) {
             break;
         } else if (respuesta.equals("n")) {
-            equipoAleatorio = true;
+            equipoManual = false;
             break;
         } else {
             System.out.println("Respuesta no válida. Por favor, ingresa 's' o 'n'.");
@@ -131,10 +66,7 @@ public class Entrenador extends SerVivo{
         }
     }
   for (int i = 0; i < 3; i++) {
-       Pokemon pokemon = new Pokemon(null, null, null, i);
-        // Agregar el Pokémon al equipo
-        equipo.add(pokemon.InstanciarPokemon(equipoAleatorio));
-        
+       equipo.add(Pokemon.InstanciarPokemon(equipoManual));
       
   }
 
@@ -149,10 +81,4 @@ public class Entrenador extends SerVivo{
     public void celebracion(){
         System.out.println("Eso es, ¡victoria!");
     }
-
-    public static ArrayList<Entrenador> getEntrenadores() {
-        return entrenadores;
-    }
-   
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Pokemon poke1 = Pokemon.InstanciarPokemon(true);
-        Pokemon poke2 = Pokemon.InstanciarPokemon(true);
-        poke1.atacar(poke2);
+        Entrenador entrenador = Entrenador.capturarEntrenador();
+        entrenador.capturarPokemon();
+        entrenador.nombrarEquipo();
     }
 }
