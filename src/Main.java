bce4b4927640f3de62a0f3310edb6e6ba0002ba1
saenz22 <saenz.sebastian@correounivalle.edu.co>
2025-04-09 18:56:10 -0500

    Agregando SerVivo.java con sus métodos y prueba en Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Batalla batalla = new batalla();
+        
+        Entrenador e1 = Entrenador.capturarEntrenador();
+        Entrenador e2 = Entrenador.capturarEntrenador();
+        Batalla.batallaPorEquipos(e1, e2);;
     }
 }
 
